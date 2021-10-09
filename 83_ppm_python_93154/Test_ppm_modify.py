8cf46e401dddefe8fe65322d5de82ccaacdb9835
Karthikai-28 <mohankarthee@gmail.com>
2021-10-09 23:15:29 +0530

    Template python updated for ppm image proessing 83rd assignment

---
+++ 83_ppm_python_93154/Test_ppm_modify.py
@@ -0,0 +1,8 @@
+import pytest
+from ppm_modify import *
+
+def test_color_translate():
+    assert False
+    
+def test_grey_scale():
+    assert False
---
+++ 83_ppm_python_93154/ppm_modify.py
@@ -0,0 +1,48 @@
+"""
+C200 Homework Assignment 6 : ppm modify
+Author: YOUR-NAME-HERE
+Date: WHEN YOU COMPLETED IT
+The goal of this assignment is to give you practice working with lists
+by writing a program that manipulates image files in various ways.
+"""
+from math import sqrt
+
+def color_translate(line):
+    # TODO:
+    # write the complete docstring of this function.
+    # For part 1
+    # Inputs a single line (string) stripped and translates
+    # the values as defined in the handout.
+    # returns a string
+    pass
+
+def process_ppm(in_filename, out_filename, filter):
+    # TODO:
+    # 1. write the complete docstring of this function.
+    # 2. implement the process function as specified in the handout
+    pass
+
+def main_part1():
+    # TODO: call the <decode> function you developed to decode
+    # the image <files/part1.ppm>
+    pass
+
+def grey_scale(line):
+    # TODO:
+    # 1. write the complete docstring of this function.
+    # 2. implement the grey_scale function as specified in the handout
+    pass
+
+def main():
+    # TODO: implement the following required items:
+    """
+    1. Ask the user for an input file.
+    2. Ask the user for an output file.
+    3. Perform grey_scale conversion on the input file and write the
+    result to the output file in ppm format (don't forget to write out
+    the header information!).
+    4. WRite the complete docstring
+    """
+    pass
+if __name__ == '__main__':
+    main() 
