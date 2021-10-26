f1cd24aedaf440af4a4b13ebe51e3a69c2d0c42d
Karthikai-28 <mohankarthee@gmail.com>
2021-10-26 07:38:52 +0530

    Two new assignments added with solution

---
+++ 90_leetcode_94227/README.md
@@ -0,0 +1,4 @@
+# LeetBot
+
+## About
+A simple bot to enable LeetCode-fueled group suffering on Slack. 
---
+++ 90_leetcode_94227/leetbot/main.py
@@ -0,0 +1,84 @@
+
+import argparse
+import logging
+
+import slack_client.client
+from leetcode.posted_questions import PostedLeetCodeQuestions
+from leetcode.questions import LeetCodeQuestions
+from leetcode.problem import LeetProblem
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--alert', required=False, dest='alert', action='store_true')
+    parser.add_argument('--channel',    '-c', required=True,  dest='channel',    action='store', help='Channel to post to. Bot must be a member to post.')
+    parser.add_argument('--data_file',  '-D', required=False, dest='data_file',  action='store', help='Where to read/write posted questions. Default="./leetbot.json"', default='leetbot.json')
+    parser.add_argument('--difficulty', '-d', required=False, dest='difficulty', action='store', help='List of any combination of [easy, medium, hard]', type=str, default='easy,medium,hard')
+
+    args = parser.parse_args()
+    args.difficulty = args.difficulty.lower()
+
+    print('Args:')
+    [print(f'\t{k}: {v}') for k, v in args.__dict__.items()]
+
+    return args
+
+
+def get_question(posted_questions: list) -> LeetProblem:
+
+    lc = LeetCodeQuestions()
+    problem = id = None
+    while id is None:
+        id = lc.get_random_problem_id(posted_questions.questions)
+        problem = lc.questions_by_id[id]
+        if problem.difficulty not in args.difficulty or problem.paid_only:
+            id = None
+
+    return problem
+
+
+def build_message(question: LeetProblem):
+    body = f"*Today's LeetCode Question incoming{' @channel' if args.alert else ''}!*\n"
+    body += f'\tName: {question.question_title.title()}\n'
+    body += f'\tID: {question.question_id}\n'
+    body += f'\tLevel: {question.difficulty.title()}\n'
+    body += f'\tURL: {question.url}\n'
+    message = {
+        "channel": args.channel,
+        "unfurl_links": True,
+        "unfurl_media": False,
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": body,
+                }
+            }
+        ]
+    }
+    return message
+
+
+def main():
+    posted_questions = PostedLeetCodeQuestions(args.data_file)
+    problem = get_question(posted_questions)
+    message = build_message(problem)
+
+    if message is None:
+        print('Nothing to post...')
+        exit()
+
+    response = slack_client.client.post_to_slack(message)
+    if response is not None:
+        posted_questions.add_posted_question_id(problem.question_id)
+        posted_questions.write_posted_questions()
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    args = parse_args()
+    main()
